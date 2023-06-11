d7f5192c55bd2ee81c3077ba0a65344c578e89ae
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2023-06-10 20:02:20 -0400

    Ajustando calculadora

--- meu_app/lib/functions/calculator.dart
+++ meu_app/lib/functions/calculator.dart
@@ -1,17 +1,76 @@
 import 'dart:convert';
 import 'dart:io';
 
-double soma(double numero1, double numero2){
-  return numero1 + numero2;
+// class calculadora{
+void calcular(double numero1, double numero2, String simbolo) {
+  print("Bem vindos a nossa calculadora");
+//  print("Informe o primeiro numero:");
+  // var linee = stdin.readLineSync(encoding: utf8);
+//var prova1 = int.tryParse(line!);
+  // var numero1 = double.tryParse(linee == null ? "0" : linee);
+
+  double soma(double numero1, double numero2) {
+    return numero1 + numero2;
+  }
+
+  double subtracao(double numero1, double numero2) {
+    return numero1 - numero2;
+  }
+
+  double multiplicacao(double numero1, double numero2) {
+    return numero1 * numero2;
+  }
+
+  double divisao(double numero1, double numero2) {
+    return numero1 / numero2;
+  }
+
+  double resultado2 = 0;
+  switch (simbolo) {
+    case "+":
+      //3  resultado2 = (numero1! + numero2!);
+      resultado2 = soma(numero1, numero2);
+      break;
+    case "-":
+      resultado2 = subtracao(numero1, numero2);
+      break;
+    case "/":
+      resultado2 = divisao(numero1, numero2);
+      break;
+    case "*":
+      resultado2 = multiplicacao(numero1, numero2);
+      break;
+
+    default:
+      print("Operacao invalida");
+      exit(0);
+  }
+  //return resultado2;
+  print("O resultado da operacao: $simbolo  da CALCULADORA e: $resultado2");
 }
 
-double subtracao(double numero1, double numero2){
-  return numero1 - numero2;
+String lerConsole(String texto) {
+  print(texto);
+  var linee = stdin.readLineSync(encoding: utf8);
+  return linee ?? "0";
 }
 
-double multiplicacao(double numero1, double numero2){
-  return numero1 * numero2;
+double lerconsoleDouble(String texto) {
+  var line = lerConsole(texto);
+  return double.parse(line);
 }
-double divisao(double numero1, double numero2){
-  return numero1 / numero2;
+
+void printName(String name) {
+  print("My name is: $name");
 }
+
+int returnNumero() {
+  return 30;
+}
+
+int sum(int num1, int num2) {
+  return (num1 + num2);
+}
+
+//}
+
--- meu_app/lib/functions/functions.dart
+++ meu_app/lib/functions/functions.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 void printHello() {
   print("Hello Word");
   print(returnNumero());
@@ -6,14 +9,76 @@ void printHello() {
   print(number);
 }
 
-void printName(String name){
+void calcu(double numero1, double numero2, String simbolo) {
+  String lerConsole(String texto) {
+    print(texto);
+    var linee = stdin.readLineSync(encoding: utf8);
+    return linee ?? "0";
+  }
+
+  print("Bem vindos a nossa calculadora");
+//  print("Informe o primeiro numero:");
+  // var linee = stdin.readLineSync(encoding: utf8);
+//var prova1 = int.tryParse(line!);
+  // var numero1 = double.tryParse(linee == null ? "0" : linee);
+  var numero1 = double.tryParse(lerConsole("Informe o primeiro numero:"));
+
+  // print("Informe o segundo numero:");
+  // var lineee = stdin.readLineSync(encoding: utf8);
+  var numero2 = double.tryParse(lerConsole("Informe o segundo numero:"));
+
+  // print("Informe a operacao: +, -, /, *");
+  // var oper = stdin.readLineSync(encoding: utf8);
+//  var simbolo = (oper == null ? "0" : oper);
+  var simbolo = (lerConsole("Informe a operacao: +, -, /, *"));
+  double soma(double numero1, double numero2) {
+    return numero1 + numero2;
+  }
+
+  double subtracao(double numero1, double numero2) {
+    return numero1 - numero2;
+  }
+
+  double multiplicacao(double numero1, double numero2) {
+    return numero1 * numero2;
+  }
+
+  double divisao(double numero1, double numero2) {
+    return numero1 / numero2;
+  }
+
+  double resultado2 = 0;
+  switch (simbolo) {
+    case "+":
+      //3  resultado2 = (numero1! + numero2!);
+      resultado2 = soma(numero1!, numero2!);
+      break;
+    case "-":
+      resultado2 = subtracao(numero1!, numero2!);
+      break;
+    case "/":
+      resultado2 = divisao(numero1!, numero2!);
+      break;
+    case "*":
+      resultado2 = multiplicacao(numero1!, numero2!);
+      break;
+
+    default:
+      print("Operacao invalida");
+      exit(0);
+  }
+  //return resultado2;
+  print("O resultado da operacao: $simbolo  da CALCULADORA e: $resultado2");
+}
+
+void printName(String name) {
   print("My name is: $name");
 }
 
-  int returnNumero() {
-    return 30;
-  }
+int returnNumero() {
+  return 30;
+}
 
-int sum(int num1, int num2){
-    return (num1+num2);
+int sum(int num1, int num2) {
+  return (num1 + num2);
 }
