c31c9d025233fc2df3b89f76ffa9c7ccb2c53e82
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2023-09-05 19:55:48 -0400

    Grupos de testes

--- meu_app/lib/service/NotificacaoInterface.dart
+++ meu_app/lib/service/NotificacaoInterface.dart
@@ -1,5 +1,4 @@
 import 'package:meu_app/classes/pessoa_abstract.dart';
-import 'package:meu_app/service/EnviarNotificacao.dart';
 
 abstract class NotificacaoInterface {
   void EnviarNotificacao(Pessoa pessoa);
